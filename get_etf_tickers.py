e80738d3f2f9a7e0c43e4e6e4eed1035aa13d48f
Simone Ruffino <simoneruffino@gmail.com>
2015-08-31 21:59:50 +0200

    found oldest ETFs, started working on volatility

--- get_etf_tickers.py
+++ get_etf_tickers.py
@@ -126,3 +126,74 @@ if __name__ == "__main__":
     #         #
     #     except IOError as e:
     #         print tickers[i] + ' :' + str(e)
+    
+    
+    '''
+    Analyse log file for start date
+    '''
+    with open('log_yahoo.txt', 'r') as fp:
+        myjson = json.load(fp)
+    fp.close()
+    df = pd.DataFrame.from_dict(myjson, orient='index')
+    df.columns = ['start_date']
+    df.sort_index(by='start_date')
+    # Filtering: start date before a certain date
+    newdf = df[df.start_date <= '2007-01-01 00:00:00'].sort_index(by='start_date')
+    
+    # printing more info on filtered ETFs
+    largedf = pd.DataFrame()
+    for item in newdf.index:
+        # etfs[etfs['Reuters RIC (Italy)'] == 'XYP1.MI']        
+        largedf = largedf.append(etfs[etfs['Reuters RIC (Italy)'] == item])
+    # adding start dates to ETF info
+    left = largedf.set_index('Reuters RIC (Italy)')
+    right = newdf
+    result = pd.concat([left, right], axis=1)
+    # writing results to file
+    result.to_csv(CSV_DIR + 'oldest_etfs.csv')
+    
+    
+    
+    '''
+    GMR seekingalpha
+    '''
+    ticker_list = ['MDY','IEV','EEM','ILF','EPP','EDV','SHY']
+    
+    for etf in ticker_list:
+        print etf
+        try:
+            # f = pandas.io.data.DataReader(etf+'.mi', "yahoo", start="1980/1/1")
+            f = pandas.io.data.DataReader(etf, "yahoo", start="1980/1/1")
+            print f.iloc[:1].to_string()
+            # extract start date and store in a dict
+            log_dict[etf] = str(f.first_valid_index())
+            # save quotes to CSV
+            f.to_csv(CSV_DIR + etf + '.csv')
+        except (IOError, UnicodeEncodeError) as e:
+            print etf + ' :' + str(e)
+            # store error in a dict
+            err_dict[etf] = str(e)
+
+    # Open a log file to track info on ETF (start date)
+    with open(CSV_DIR + 'log_yahoo_GMR.txt', 'w') as logfile:
+        json.dump(log_dict, logfile, indent=4)
+    logfile.close()
+
+    # Open a log file to track Yahoo quote download errors
+    with open(CSV_DIR + 'err_yahoo_GMR.txt', 'w') as errfile:
+        json.dump(err_dict, errfile, indent=4)
+    errfile.close()
+
+    print '========== END ========='
+    print 'ETFs downloaded: ' + str(len(log_dict))
+    print 'ETFs NOT downloaded: ' + str(len(err_dict))
+    
+    
+    
+
+
+    
+    
+    
+    
+    
